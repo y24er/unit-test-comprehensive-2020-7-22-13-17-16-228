5113857f1428965ed0bdc32851c1a1948e2008e8
y24er <1028704486@qq.com>
2020-07-24 01:00:34 +0800

    implement given_answer_is_1234_and_guess_number_is_1025 when guess number then 1A1B

--- src/main/java/example/GuessNumberGame.java
+++ src/main/java/example/GuessNumberGame.java
@@ -1,6 +1,8 @@
 package example;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class GuessNumberGame {
     AnswerGenerator answerGenerator;
@@ -13,9 +15,32 @@ public class GuessNumberGame {
         int[] answer = answerGenerator.generator();
         if (Arrays.equals(answer, guessNumber)) {
             return "4A0B";
+        } else {
+            return count(guessNumber);
         }
 
-        return null;
+    }
+
+    public String count(int[] guessNumbers) {
+        int countA = 0;
+        int countB = 0;
+        List<Integer> answerNoIncludeTheSame = new ArrayList<>();
+        List<Integer> guessNoIncludeTheSame = new ArrayList<>();
+
+        int[] answers = answerGenerator.generator();
+        for (int index = 0; index < guessNumbers.length; index++) {
+            if (answers[index] == guessNumbers[index]) {
+                countA++;
+            } else {
+                answerNoIncludeTheSame.add(answers[index]);
+                guessNoIncludeTheSame.add(guessNumbers[index]);
+            }
+        }
+        for (int index = 0; index < answerNoIncludeTheSame.size(); index++) {
+            if (answerNoIncludeTheSame.contains(answerNoIncludeTheSame.get(index)))
+                countB++;
+        }
+        return countA + "A" + countB + "B";
     }
 
 }
