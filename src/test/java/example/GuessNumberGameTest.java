3a61d150ca38f327ef9ed186891aa5956eb444ac
y24er <1028704486@qq.com>
2020-07-24 00:35:18 +0800

    implement should_return_4A0B_when_guess_number_given_answer_is_1234_and_guess_number_is_1234 test case

---
+++ src/test/java/example/GuessNumberGameTest.java
@@ -0,0 +1,24 @@
+package example;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.BDDMockito.given;
+
+public class GuessNumberGameTest {
+    @Test
+    void should_return_4A0B_when_guess_number_given_answer_is_1234_and_guess_number_is_1234() {
+        //given
+        AnswerGenerator morkAnswerGenerator = Mockito.mock(AnswerGenerator.class);
+        given(morkAnswerGenerator.generator()).willReturn(new int[]{1, 2, 3, 4});
+        int[] guessNumber = {1, 2, 3, 4};
+
+        //when
+        GuessNumberGame guessNumberGame = new GuessNumberGame(morkAnswerGenerator);
+        String actual = guessNumberGame.guess(guessNumber);
+
+        //then
+        assertEquals("4A0B", actual);
+    }
+}
