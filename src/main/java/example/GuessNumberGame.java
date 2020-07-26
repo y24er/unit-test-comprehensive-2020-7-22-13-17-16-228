df624e2126cfdbe256f082d4e7fa2cb2a3286666
y24er <1028704486@qq.com>
2020-07-26 14:52:01 +0800

    [GuessNumberGame][Jessie] refactor: refactor the count method and guess method

--- src/main/java/example/GuessNumberGame.java
+++ src/main/java/example/GuessNumberGame.java
@@ -1,10 +1,9 @@
 package example;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
-public class GuessNumberGame {
+public class GuessNumberGame implements AnswerGenerator {
     private int[] answers;
 
     public GuessNumberGame(AnswerGenerator morkAnswerGenerator) {
@@ -12,33 +11,36 @@ public class GuessNumberGame {
     }
 
     public String guess(int[] guessNumbers) {
-        if (Arrays.equals(answers, guessNumbers)) {
-            return "4A0B";
-        } else {
-            return count(guessNumbers);
-        }
+        return count(guessNumbers);
 
     }
 
     public String count(int[] guessNumbers) {
         int countA = 0;
         int countB = 0;
-        List<Integer> answerNoIncludeTheSame = new ArrayList<>();
-        List<Integer> guessNoIncludeTheSame = new ArrayList<>();
 
+        List<Integer> answerList = new ArrayList<>();
+        for (int answer : answers) {
+            answerList.add(answer);
+        }
         for (int index = 0; index < guessNumbers.length; index++) {
             if (answers[index] == guessNumbers[index]) {
                 countA++;
             } else {
-                answerNoIncludeTheSame.add(answers[index]);
-                guessNoIncludeTheSame.add(guessNumbers[index]);
+                if (answerList.contains(guessNumbers[index]))
+                    countB++;
             }
-        }
-        for (int index = 0; index < answerNoIncludeTheSame.size(); index++) {
-            if (answerNoIncludeTheSame.contains(guessNoIncludeTheSame.get(index)))
-                countB++;
         }
         return countA + "A" + countB + "B";
     }
 
+    @Override
+    public int[] generator() {
+        int[] answers = new int[4];
+        for (int i = 0; i < 4; i++) {
+            int answer = (int) (Math.random() * 9);
+            answers[i] = answer;
+        }
+        return answers;
+    }
 }
